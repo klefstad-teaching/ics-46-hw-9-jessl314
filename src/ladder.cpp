bb5996f85e647d5bda54a7066b45dfa5b8807107
jessl314 <jlchrysanthemum456@gmail.com>
2025-03-10 04:33:39 +0000

    fixed the ladder errors

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -6,6 +6,9 @@ void error(string word1, string word2, string msg) {
 
 
 bool sameLenDiff(const string& word1, const string& word2) {
+    if (word1 == word2) {
+        return true;
+    }
     int numDiff = 0;
     for (int i=0; i < static_cast<int>(word1.size()); ++i) {
         if (word1[i] != word2[i]) {
@@ -67,17 +70,13 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
         for (int j = 1; j <= len2; ++j) {
             if (str1[i - 1] == str2[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1]; 
-            } else {
-
+            } 
+            else {
                 dp[i][j] = min({
                     dp[i - 1][j] + 1,
                     dp[i][j - 1] + 1,
                     dp[i - 1][j - 1] + 1
                 });
-            }
-            // Early exit if distance exceeds the limit d
-            if (dp[i][j] > d) {
-                return false;
             }
         }
     }
@@ -129,9 +128,14 @@ void load_words(set<string> & word_list, const string& file_name) {
 }
 
 void print_word_ladder(const vector<string>& ladder) {
+    if (ladder.empty()) {
+        cout << "No ladder found." << endl;
+        return;
+    }
     for (const string& word : ladder) {
         cout << word << " ";
     }
+    cout << "\n";
 }
 
 void verify_word_ladder(const vector<string>& ladder) {
